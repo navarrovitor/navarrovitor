0ef32c7b1402d75447e184ca25ef0ac35631ee9d
Vitor Sant'Ana Navarro <43391367+navarrovitor@users.noreply.github.com>
2022-01-26 16:27:01 -0300

    Update and rename whyAreYouHere.cs to goBack.cs

---
+++ .github/workflows/stuff/important_things/classified/top_secret/area52_kind_of_things/I_mean_it/goBack.cs
@@ -0,0 +1,9 @@
+namespace goBack
+{
+    class GoBack {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("I really mean it. Why are you here? Why do you want to get so down in the mud?");
+        }
+    }
+}
--- .github/workflows/stuff/important_things/classified/top_secret/area52_kind_of_things/I_mean_it/whyAreYouHere.cs
+++
@@ -1,9 +0,0 @@
-namespace HelloWorld
-{
-    class Hello {         
-        static void Main(string[] args)
-        {
-            System.Console.WriteLine("I really mean it. Why do you want to get so down in the mud?");
-        }
-    }
-}
