6a7d3fb7cd46dd0664dc6a3bd251bfec0b1b64f9
Vitor Sant'Ana Navarro <43391367+navarrovitor@users.noreply.github.com>
2022-01-26 16:26:10 -0300

    Create whyAreYouHere.cs

---
+++ .github/workflows/stuff/important_things/classified/top_secret/area52_kind_of_things/I_mean_it/whyAreYouHere.cs
@@ -0,0 +1,9 @@
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("I really mean it. Why do you want to get so down in the mud?");
+        }
+    }
+}
